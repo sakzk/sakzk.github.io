6216952e7d81b0773ef2013b4ca3e2dd665ba6f7
dolphinq <kazukisakamot@gmail.com>
2024-02-29 01:41:15 +0900

    タイミングによってファイルの内容が上書きされる不具合を修正

--- create_article_file.sh
+++ create_article_file.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # ファイル名を現在の日付から生成
-filename=$(date +'_posts/%Y-%m-%d-foo').md
+filename=$(date +'_posts/%Y-%m-%d-%foo').md
+
+# ファイルが既に存在するか確認
+if [ -e "$filename" ]; then
+    echo "ファイル '$filename' は既に存在しています。何もせずに終了します。"
+    exit 0
+fi
 
 # ファイルに書き込むテキストを生成
 content="---
@@ -13,7 +19,7 @@ date:   $(date +'%Y-%m-%d %H:00:00 %z')
 #"
 
 # ファイルにテキストを書き込み
-echo -e "$content" > "$filename"
+echo -e "$content" >> "$filename"
 
 # 結果を表示
 echo "ファイル '$filename' が作成されました。"
